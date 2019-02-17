15cfd74b818516952e8e9a4cb97109d428b8f8f1
Amit Bhoyar <amitandme@gmail.com>
2019-02-17 16:57:35 -0500

    external api to get currency exchange rate integrated.

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,5 @@
-import {Request, Response, response} from 'express';
+import {Request, Response} from 'express';
+import * as request from 'request'
 
 export class MainController {
     constructor(){
@@ -6,6 +7,18 @@ export class MainController {
     }
 
     public async getCurrencyData(req : Request, res: Response){
-        res.json({data : "70.01"}).status(200);
-    }
+        //res.json({data : "70.01"}).status(200);
+            request({
+              uri: 'https://api.exchangeratesapi.io/latest?base=USD',
+              qs: {
+              }
+            }, (error, response, body)=>{
+                if (!error && response.statusCode === 200) {
+                    res.json(body);
+                  } else {
+                    res.json(error);
+                  }
+            })
+
+        }
 }
